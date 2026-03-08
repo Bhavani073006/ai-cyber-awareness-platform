93a6bb6903a53ef9bd3a4e2f876bc6947a9a08b1
Bhavani <bhavanibhanu3456@gmail.com>
2026-03-08 19:29:10 +0530

    app.py

--- app.py
+++ app.py
@@ -387,4 +387,4 @@ def trusted_capture():
                              unknown_email=email)
 if __name__ == "__main__":
     init_db()
-    app.run(host="0.0.0.0", port=10000)True)
+    app.run(host="0.0.0.0", port=10000)
